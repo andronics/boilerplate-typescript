c9b880c56fcc593f50aed9db4f4a993cac49a966
Stephen Cox <andronics@gmail.com>
2020-04-05 13:24:34 +0100

    Revert "initial"
    This reverts commit 2cdee5b03cdc275052f2945b2d7fecebe3dfbdcc.

--- lib/components/cookie.ts
+++
@@ -1,7 +0,0 @@
-import { MemoryCookieStore } from 'tough-cookie'
-import rp from 'request-promise'
-
-export const store = new MemoryCookieStore()
-
-export const jar = rp.jar(store)
- 
--- lib/index.ts
+++ lib/index.ts
@@ -1,29 +1,3 @@
-import authorize from './components/authorize'
-
-import FundSpider from './spiders/fund'
-import SearchSpider from './spiders/search'
-
-// let companies = new SearchSpider("17460")
-// let individuals = new SearchSpider("17461")
-// let organisations = new SearchSpider("17462")
-// let statutory = new SearchSpider("17463")
-    
-authorize().then(auth => {
-
-    // new SearchSpider({ funderType: [17460, 17461, 17462, 17463] }).run()
-
-    let funds = require('../cache/cache-saw1metg.json')
-    let fundSpider = new FundSpider()
-
-    let asyncForEach = async (array: any[], callback: Function) => {
-        for (let idx = 0; idx < array.length; idx++) {
-            await callback(array[idx], idx, array)
-        }
-    }
-
-    asyncForEach(funds, async (fund: any, idx: number, arr: any[]) => {
-        console.log("[%s of %s] %s at %s", idx + 1, arr.length, fund.name, fund.url)
-        await fundSpider.fetch(fund.url)
-    })
-    
-})
+export default function(): string {
+    return "Hello World"
+}
